a560345a53f1229483ecd6b60d2421e41bb17979
Ihtisham-Khattak <ikpashteen1@gmail.com>
2024-03-10 23:09:56 +0500

    Remove redundant data from the dashboard and all other

---
+++ src/views/Reservation.js
@@ -0,0 +1,134 @@
+import {
+  Badge,
+  Card,
+  CardHeader,
+  CardFooter,
+  Media,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Table,
+  Container,
+  Row,
+  Button,
+} from "reactstrap";
+// core components
+import OnlyHeader from "components/Headers/OnlyHeader.js";
+
+const Reservation = () => {
+  return (
+    <>
+      <OnlyHeader />
+      {/* Page content */}
+      <Container className="mt--7" fluid>
+        {/* Table */}
+        <Row>
+          <div className="col">
+            <Card className="shadow">
+              <CardHeader className="border-0">
+                <h3 className="mb-0">Reservation</h3>
+              </CardHeader>
+              <Table className="align-items-center table-flush" responsive>
+                <thead className="thead-light">
+                  <tr>
+                    <th scope="col">Project</th>
+                    <th scope="col">Budget</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Action</th>
+                    <th scope="col" />
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">
+                      <Media className="align-items-center">
+                        <Media>
+                          <span className="mb-0 text-sm">
+                            Argon Design System
+                          </span>
+                        </Media>
+                      </Media>
+                    </th>
+                    <td>$2,500 USD</td>
+                    <td>
+                      <Badge color="" className="badge-dot mr-4">
+                        <i className="bg-warning" />
+                        pending
+                      </Badge>
+                    </td>
+                    
+                    <td>
+                      <Badge color="" className="badge-dot mr-4">
+                        <Button>Edit</Button>
+                      </Badge>
+                      <Badge color="" className="badge-dot mr-4">
+                        <Button>Delete</Button>
+                      </Badge>
+                    </td>
+                  </tr>
+                
+                </tbody>
+              </Table>
+
+
+              <CardFooter className="py-4">
+                <nav aria-label="...">
+                  <Pagination
+                    className="pagination justify-content-end mb-0"
+                    listClassName="justify-content-end mb-0"
+                  >
+                    <PaginationItem className="disabled">
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                        tabIndex="-1"
+                      >
+                        <i className="fas fa-angle-left" />
+                        <span className="sr-only">Previous</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem className="active">
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        1
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        2 <span className="sr-only">(current)</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        3
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <i className="fas fa-angle-right" />
+                        <span className="sr-only">Next</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                  </Pagination>
+                </nav>
+              </CardFooter>
+            </Card>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Reservation;
